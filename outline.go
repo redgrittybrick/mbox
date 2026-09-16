431b0733f9b9b9f59cca075915bda19323306b81
ian <ian@giant.home>
2026-09-16 17:20:37 +0100

    minor formatting

--- outline.go
+++ outline.go
@@ -1,10 +1,10 @@
 package mbox
 
 import (
-	"github.com/emersion/go-message"
-	_ "github.com/emersion/go-message/charset"
 	"io"
 	"strings"
+	"github.com/emersion/go-message"
+	_ "github.com/emersion/go-message/charset"
 )
 
 func Outline(msg string) (string, string) {
@@ -18,7 +18,6 @@ func Outline(msg string) (string, string) {
 	if message.IsUnknownCharset(err) { // This error is not fatal
 		sbo.WriteString("Unknown encoding: " + err.Error() + "\n")
 	} else if err != nil {
-		//log.Fatal(err)
 		sbo.WriteString("Error: " + err.Error() + "\n")
 		return sbo.String(), sbt.String()
 	}
@@ -39,19 +38,19 @@ func Outline(msg string) (string, string) {
 			switch t {
 			case "text/plain", "text/html":
 				/*
-					b, _ := io.ReadAll(p.Body)
-					sbt.WriteString("================ text part ================\n")
-					sbt.WriteString(string(b))
-					sbt.WriteString("-------------------------------------------\n")
+				b, _ := io.ReadAll(p.Body)
+				sbt.WriteString("================ text part ================\n")
+				sbt.WriteString(string(b))
+				sbt.WriteString("-------------------------------------------\n")
 				*/
 			case "multipart/alternative":
 				// BUG - doing io.ReadAll(p.Body) here steals from the library parse
-
+				
 				b, _ := io.ReadAll(p.Body)
-				o, t := Outline(string(b))
+				o,t := Outline(string(b))
 				sbo.WriteString(o)
 				sbt.WriteString(t)
-
+				
 			}
 
 		}
@@ -62,10 +61,10 @@ func Outline(msg string) (string, string) {
 		case "text/plain", "text/html":
 			// BUG
 			/*
-				b, _ := io.ReadAll(m.Body)
-				sbt.WriteString("================ text part ================\n")
-				sbt.WriteString(string(b))
-				sbt.WriteString("-------------------------------------------\n")
+			b, _ := io.ReadAll(m.Body)
+			sbt.WriteString("================ text part ================\n")
+			sbt.WriteString(string(b))
+			sbt.WriteString("-------------------------------------------\n")
 			*/
 		}
 	}
