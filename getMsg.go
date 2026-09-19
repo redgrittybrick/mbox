888369c8d65fa0b8abf6b81c4bc730a1f62937d7
redgrittybrick <ian@redgrittybrick.org>
2026-09-19 10:36:19 +0100

    Removed commented out debugging code

--- getMsg.go
+++ getMsg.go
@@ -8,7 +8,6 @@ import (
 
 // reads mbox file skipping to specific message
 func GetMsg(path string, startByte, endByte int64) (string, error) {
-	//log.Printf("Seeking %d %d in '%s'", startByte, endByte, path)
 
 	f, err := os.Open(path)
 	if err != nil {
@@ -16,15 +15,9 @@ func GetMsg(path string, startByte, endByte int64) (string, error) {
 	}
 	defer f.Close()
 
-	o, err := f.Seek(startByte, io.SeekStart)
+	_, err := f.Seek(startByte, io.SeekStart)
 	if err != nil {
 		return "", err
-	}
-	if o != endByte {
-		/*
-			return "",
-				fmt.Errorf("File '%s' Seek %d reached %d instead", path, endByte, o)
-		*/
 	}
 
 	l := int(endByte - startByte + 1)
