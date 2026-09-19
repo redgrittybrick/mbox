914cb9da43857af7a5a13c06a63bfdec3c2d7142
redgrittybrick <ian@redgrittybrick.org>
2026-09-19 10:52:01 +0100

    Add descriptive comment for log()

--- log.go
+++ log.go
@@ -4,6 +4,7 @@ import (
 	"log"
 )
 
+// Writes a log message if log level > 0 (see parser.LogLevel(n))
 func (p Parser) log(format string, v ...interface{}) {
 	if p.logLevel > 0 {
 		log.Printf("mbox "+format, v...)
