5c5d9e6969fb518a7f2c80e2cef48dbdc87f2120
redgrittybrick <ian@redgrittybrick.org>
2026-09-19 10:38:42 +0100

    Removed unused code

--- header.go
+++ header.go
@@ -1,15 +1,10 @@
 package mbox
 
 import (
-	//	"bufio"
 	"fmt"
-	//	"log"
 	"os"
-	// "strconv"
-	// "strings"
 )
 
-// -------------------------------------------------------------
 // extracted data - also see type MsgInfo in indexFile.go
 
 type header struct {
@@ -34,7 +29,6 @@ func (h *header) Reset() {
 	h.contentType = ""
 }
 
-// -------------------------------------------------------------
 // data in form we want to write to indexes
 
 const fileIndexName = "message-files.csv"
@@ -46,37 +40,6 @@ type fileInfo struct {
 
 type fileInfos map[rune]fileInfo
 
-/*
-func (fis *fileInfos) Read() {
-	m := *fis
-	f, err := os.Open(fileIndexName)
-	if err != nil {
-		log.Printf("Can't read file index %q because %v", fileIndexName, err)
-		return
-	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := scanner.Text()
-		items := strings.Split(line, "|")
-		if len(items) == 3 {
-			keyStr := items[0]
-			key := rune(keyStr[0])
-			path := items[1]
-			size, err := strconv.Atoi(items[2])
-			check(err, "Atoi("+items[2]+")")
-			m[key] = fileInfo{path: path, size: size}
-			log.Printf("Read File Info %c : %s (%d)", key, m[key].path, m[key].size)
-		}
-	}
-	err = scanner.Err()
-	if err != nil {
-		log.Printf("Error reading file index %q : %v", fileIndexName, err)
-	}
-
-}
-*/
-
 func (fis fileInfos) Write() {
 	f, err := os.Create(fileIndexName)
 	check(err, "Creating "+fileIndexName)
@@ -85,28 +48,3 @@ func (fis fileInfos) Write() {
 		fmt.Fprintf(f, "%d|%s|%d\n", k, fi.path, fi.size)
 	}
 }
-
-// ...............................................................
-
-/*
-type messageInfo struct {
-	fileRef             rune
-	from, date, subject string
-	size                int
-}
-
-func (mi messageInfo) String() string {
-	items := []string{string(mi.fileRef), mi.from, mi.date, mi.subject, strconv.Itoa(mi.size)}
-	return strings.Join(items, "|")
-}
-
-type messageInfos []messageInfo
-
-func (mis *messageInfos) Read() {
-
-}
-
-func (mis messageInfos) Write() {
-
-}
-*/
